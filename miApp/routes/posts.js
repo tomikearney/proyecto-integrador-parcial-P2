fb45c571d970daecf48cf292b7749ebb06caea74
OriCacsire <Ocacsiregeronimo@udesa.edu.ar>
2023-09-10 12:36:22 -0300

    le cambie usuarios a users para acortar la ruta. De esta forma se dirige al perfil correspondiente desde inicio

--- miApp/routes/posts.js
+++ miApp/routes/posts.js
@@ -8,7 +8,7 @@ var router = express.Router();
 /* GET users listing. */
 
 /*se agregaria un post cuando se ingrese un id de un usuario indicado, y tambien tenermos que validar de que pasaria cunado no este el id */
-router.get("/detalle/id/:id", postsController.detallePost);/*?? */
+router.get("/detalle/id/:id", postsController.detallePost);
 
 router.get("/add", postsController.agregarPost);
 
