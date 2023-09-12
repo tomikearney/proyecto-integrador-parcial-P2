e4ce3dcf2eb897b41b62c2fb3ee91ddb3cb50581
tomikearney <tomikearney@hotmail.com>
2023-09-12 19:46:06 -0300

    Ya está la estructura del sql falta completar con la info

--- miApp/db/data.sql
+++ miApp/db/data.sql
@@ -46,6 +46,65 @@ INSERT INTO usuarios VALUES(DEFAULT, "Oriana Benasconi", "oriana@example.com", "
 INSERT INTO usuarios VALUES(DEFAULT, "Tomas Michel", "tomas@example.com", "sTudyPass22", "/img/tomas.png", "2000-09-19", 47087649, DEFAULT, DEFAULT, DEFAULT);
 INSERT INTO usuarios VALUES(DEFAULT, "Jorge Perez", "jorge@example.com", "postPass22", "/img/jorge.png", "2000-09-09", 66798539, DEFAULT, DEFAULT, DEFAULT);
 
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "5 libros de ficción en inglés", "Si estas comenzando a leer en inglés, a continuación te dejamos algunas opciones entretenidas", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Novedad Septiembre", "¡Los mejores libros ingresarón este mes! ¿Ya tenes el tuyo?", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Infantiles", "En este mes se incorporarón NUEVOS libros para los pequeños gigantesss", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "LecturaCooperativa", "En este espacio se podra seleccionar un libro que nos guste a todos y poder hacer una lectura compartida.¿Les gusta la idea? Proximamente subimos las fechas", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Recomendacion", "En el día de hoy les venimos a recomendar 2 libros de Ficción. Por un lado De Ira y Amor de Sabaa Tahir y por el otro Valientemente de Maggie Stiefvater", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Recomendacion Fantasia", "En el día de hoy les venimos a recomendar 2 libros de Fantasía. Por un lado El Asesinato del Señor Wickham de Claudia Gray y por el otro Una Magia Impregnada de Veneno de Judy l. Lin", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Cuestionamiento1", "Ayudanos a armar una playlist conjunta. Pero antes queremos saber si ustedes leer con música. Dejanos tu respuesta en los comentarios!", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "OffBooks", "¡Septiembre es el mes perfecto para sumergirse en un buen libro en Winter Libros! Disfruta de un 10% de descuento en todos nuestros libros, además de envío GRATIS a tu sucursal más cercana en pedidos superiores a 14500.", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "Imperdibles", "5 Libros que no podés dejar pasar ¿Cuál agregarias?", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO posteos VALUES(DEFAULT, DEFAULT, "HarryPotter", "Descuentos Especiales en todos los libros de la saga de Harry Potter ¿Leístes toda la saga? ¿Qué te parecio?", DEFAULT, DEFAULT, DEFAULT);
 
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES(DEFAULT, DEFAULT, DEFAULT, "", DEFAULT, DEFAULT, DEFAULT);
 
 /*INSERTAR DATOS*/
