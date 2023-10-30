323190cccc0e016939d2075a73da92569c3c500a
tomikearney <tomikearney@hotmail.com>
2023-10-30 10:00:07 -0300

    Cambie el nombreImg en la base de datos por la URL

--- miApp/database/config/config.js
+++ miApp/database/config/config.js
@@ -1,21 +1,21 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": null,//CAMBIAR CONTRASEÑA DE MAC
+    "password": "root" ,//CAMBIAR CONTRASEÑA DE MAC
     "database": "data_bases",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null, //CAMBIAR CONTRASEÑA DE MAC
+    "password": "root", //CAMBIAR CONTRASEÑA DE MAC
     "database": "data_bases",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,//CAMBIAR CONTRASEÑA DE MAC
+    "password": "root",//CAMBIAR CONTRASEÑA DE MAC
     "database": "data_bases",
     "host": "127.0.0.1",
     "dialect": "mysql"
